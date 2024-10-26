51e4ecd0fe3edb26a434b35d8769d2d4422ac390
Arhan <128213818+Arhznblaster@users.noreply.github.com>
2024-10-26 19:10:19 +0600

    install dependecies

--- setup.sh
+++ setup.sh
@@ -1,1 +1,15 @@
-echo "works"
+#!/bin/bash
+
+echo "• | Welcome to the Unifier Instant Setup script!"
+
+echo "• | Hang tight while we install some things."
+
+echo "• | Creating temporary folder for installation process."
+mkdir -p "$HOME/.unifiersetuptemp"
+
+echo "• | Creating folder for the bot. This can be moved if needed."
+mkdir -p "$HOME/unifierbot"
+
+echo "• | Downloading dependency list."
+curl -o "$HOME/requirements.txt" https://raw.githubusercontent.com/UnifierHQ/unifier/refs/heads/main/requirements.txt | pip install -r requirements.txt
+
